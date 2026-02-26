c538bcd6b6efc83e031de8d25d279619ef640444
Bora-Valar-Kara <bora.kara.99@gmail.com>
2026-02-26 13:04:59 +0100

    middleearth

--- labs/lab5/Code/src/types.ts
+++ labs/lab5/Code/src/types.ts
@@ -1,9 +1,35 @@
-import type { Hypothesis, SpeechStateExternalEvent } from "speechstate";
-import type { ActorRef } from "xstate";
+import { Hypothesis, SpeechStateExternalEvent } from "speechstate";
+import { AnyActorRef } from "xstate";
 
-export interface DMContext {
-  spstRef: ActorRef<any, any>;
-  lastResult: Hypothesis[] | null;
+export interface Entity { // This is the type of the entities array in the NLUObject. 
+  category: string;
+  text: string;
+  confidenceScore: number;
+  offset: number;
+  length: number;
 }
 
-export type DMEvents = SpeechStateExternalEvent | { type: "CLICK" } | { type: "DONE" };
+export interface Intent { // This is the type of the intents array in the NLUObject.
+  category: string;
+  confidenceScore: number;
+}
+
+export interface NLUObject { // This is the type of the interpretation in the DMContext.
+  entities: Entity[];
+  intents: Intent[];
+  projectKind: string;
+  topIntent: string;
+}
+
+
+export interface DMContext { // This is the type of the context in the DM machine.
+  spstRef: AnyActorRef;
+  lastResult: Hypothesis[] | null;
+  confirm: boolean | null;
+  Person: string | null;
+  Day: string | null;
+  Hour: string | null;
+  interpretation: NLUObject | null;
+}
+
+export type DMEvents = SpeechStateExternalEvent | { type: "CLICK" };
